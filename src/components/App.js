4b36c9ba5df0e0dc8b142768939cea1fb6da4afd
Darren <ninenumbproductions@gmail.com>
2018-09-21 19:52:28 -0700

    boilterplate dialoge comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Dialogue } from './Dialogue';
 
 class App extends Component {
   render() {
+    let meeting = 'this is the meeting text'
     return (
       <div>
         <p>Hi</p>
+        <Dialogue 
+          text={meeting}
+        />
       </div>
     );
   }
---
+++ src/components/Dialogue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Dialogue = props => {
+    return (
+      <div>
+        <p>Dialogue Component</p>
+        {props.text }
+      </div>
+    );
+}
+
+export default Dialogue;
