1e63f30548c02b5772e8abde824613d359066809
Ryan Murry <ryanjmurry@gmail.com>
2018-09-21 20:17:59 -0700

    get routing working

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { Dialogue } from './Dialogue';
+import LandingPage from './LandingPage';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      name: '',
+      bar: 0,
+      drunkLevel: 0
+    }
+    this.handleNameSubmission = this.handleNameSubmission.bind(this);
+  }
+
+  handleNameSubmission(name) {
+    this.setState({
+      name: name
+    })
+  }
+
   render() {
     let meeting = 'this is the meeting text'
     return (
       <div>
+        <Switch>
+          <Route exact path='/' render={()=><LandingPage
+            onNameSubmission={this.handleNameSubmission} />}
+          />
+        </Switch>
         <p>Hi</p>
         <Dialogue 
           text={meeting}
---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      Working!
+    </div>
+  );
+}
+
+export default LandingPage;
