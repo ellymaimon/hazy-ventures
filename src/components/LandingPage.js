badee869e126e23453aa568824e07d3d2d8661fd
Ryan Murry <ryanjmurry@gmail.com>
2018-09-21 20:21:37 -0700

    got input working

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
+
+  const handleNameSubmission = (e) => {
+    e.preventDefault();
+    let userName = e.target.name.value;
+    props.onNameSubmission(userName);
+  }
+
   return (
     <div>
-      Working!
-    </div>
+        <h1>Hazy Ventures Game</h1>
+        <form onSubmit={handleNameSubmission}>
+          <input name="name"/>
+          <button type="submit">Start Game</button>
+        </form>
+      </div>
   );
 }
 
